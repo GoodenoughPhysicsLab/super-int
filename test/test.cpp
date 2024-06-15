6e64e5e63e167cd01a10d5a7c9b04ab05b8fc40c
Goodenough <2381642961@qq.com>
2024-06-15 10:17:41 +0800

    add operator==

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,16 @@
+#include <cassert>
+#include <cstdint>
+
 #include "../bigint/bigint.hh"
 
+void test_shlr() {
+    auto a = si::BigInt<uint8_t>(0xFF);
+    a >> 1;
+    assert(a == 0x7F);
+}
+
 int main() noexcept {
+    test_shlr();
 
     return 0;
 }
